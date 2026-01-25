4905e08b506d1cad1a63b8c45446ce24f23b32aa
𝐒𝐀𝐒𝐒𝐘𝐰𝐚𝐫𝐧𝐞𝐫 <sekaricmia@gmail.com>
2026-01-25 16:55:01 +0100

    Update script_1.js

--- Seite 1/script_1.js
+++ Seite 1/script_1.js
@@ -1,3 +1,15 @@
-document.getElementById('buttonImg').addEventListener('click', () => {
-  window.location.href = 'nextpage.html'; // put the URL of your next page here
+document.querySelectorAll('.sidenav a').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // stop page jump
+
+    const section = link.dataset.section;
+    console.log('Clicked:', section);
+  });
 });
+
+const user = localStorage.getItem("opor_current_user");
+const users = JSON.parse(localStorage.getItem("opor_users"));
+
+users[user].data.points = value;
+
+localStorage.setItem("opor_users", JSON.stringify(users));
